b307ad2b92a0abef2d4392e8f20d0840d5d68a67
achien05 <achien@binghamton.edu>
2026-09-02 15:23:28 -0400

    added to attribute and developed sheet creation (just check the changes)

--- DnD Character Sheet Scanner/Java Attempt/Attribute.java
+++ DnD Character Sheet Scanner/Java Attempt/Attribute.java
@@ -7,8 +7,20 @@ public class Attribute{
     private String src = "";
     private String prereq = "";
     private int uses;
+    private int attrID;
 
-    public Attribute(){
+    public Attribute(String name, int uses, int attrID){
+        this.name = name;
+        this.uses = uses;
+        this.attrID = attrID;
+    }
+    public Attribute(String name, int uses, int attrID, Modifier[] modifiers){
+        this.name = name;
+        this.uses = uses;
+        this.attrID = attrID;
+        for(int i = 0; i < modifiers.length;i++){
+            this.modifiers.add(modifiers[i]);
+        }
     }
 
     public String getName(){
@@ -44,7 +56,10 @@ public class Attribute{
     public ArrayList<Modifier> getModifiers(){  //provides copies for use(modification or standard usage)
         return new ArrayList<Modifier>(modifiers);
     }
-    public void setModifier(Modifier modifier){ //will replace the modifier with the same (except for a minor change)
+    public void addModifier(Modifier modifier){
+        modifiers.add(modifier);
+    }
+    public void updateModifier(Modifier modifier){ //will replace the modifier with the same (except for a minor change)
         for(int i = 0; i<modifiers.size(); i++){
             if(modifiers.get(i).getName().equals(modifier.getName())){
                 modifiers.set(i, modifier);
@@ -58,6 +73,19 @@ public class Attribute{
                 modifiers.remove(i);
                 break;
             }
+        }
+    }
+    public int getAttrID(){
+        return this.attrID;
+    }
+    public void setAttrID(int attrID){
+        this.attrID = attrID;
+    }
+
+    public void applyAttrMods(){
+        for(int i = 0; i<modifiers.size(); i++){
+            modifiers.get(i).getRelStat();
+            modifiers.get(i).getValue();
         }
     }
 }
--- DnD Character Sheet Scanner/Java Attempt/CharSheet.java
+++ DnD Character Sheet Scanner/Java Attempt/CharSheet.java
@@ -1,11 +1,23 @@
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class CharSheet{
+    private String name;
+    private int[] classLevels = new int[13];
+    private int[][] abilityScores = new int[6][7];
+    private int[][] spellSlots = new int[20][9];
+    private int totalLevels;
+    private int profBonus;
+    private ArrayList<Item> equippedGear = new ArrayList<Item>();
+    private ArrayList<Item> inventory = new ArrayList<Item>();
+    private Attribute race, clas, bg;
+
     public static void main(String[] args){
+    //command line argument parsing
         String directoryStr;
         if(args.length < 0){
             System.out.println("No path found. Using default path: ./saves");
@@ -19,6 +31,7 @@ public class CharSheet{
             System.out.println("Invalid directory path. Using default path: ./saves");
             directoryPath = new File("./saves");
         }
+    //start of program (find adequate files, then select or create)
         System.out.println("Welcome to the DnD Character Sheet Hub.");
         System.out.println("Which character would you like to resume?");
         File selectedSheet = CharSheet.selectCharacterSheet(directoryPath);
@@ -40,6 +53,7 @@ public class CharSheet{
             for(int i = 0; i<foundSheets.length; i++){
                 System.out.println(i+1 + ": " + foundSheets[i].getName());
             }
+            //user input
             try{
                 response = scanner.nextInt()-1;
                 if(response >=0 && response < foundSheets.length){
@@ -49,10 +63,21 @@ public class CharSheet{
                         return foundSheets[response];
                     }
                 }else if(response == -1){
-                    System.out.println("You have chosen none of them. Would you like to create a new sheet? (Y/y to confirm)");
+                    System.out.println("You have chosen none of them. Would you like to create a new sheet? (Y/y to confirm, otherwise quit)");
                     confirmation = scanner.nextLine();
                     if(confirmation.equals("Y") || confirmation.equals("y")){
-                        CharSheet.createCharacterSheet();
+                        CharSheet newCharacter = null;
+                        while(newCharacter == null){
+                            newCharacter = CharSheet.createCharacterSheet();
+                            if(newCharacter == null){
+                                System.out.println("Character creation failed. Trying again.");
+                                continue;
+                            }else{
+                                System.out.println("Character creation complete. Saving.");
+                                //save as file
+                                return null; //replace with path
+                            }
+                        }
                     }else{
                         return null;
                     }
@@ -66,9 +91,46 @@ public class CharSheet{
             }finally{
                 scanner.close();
             }
+            //end user input (for eventual UI retrofit)
         }
     }
 
-    public static void createCharacterSheet(){  //wip change from void to File upon development
+    public static CharSheet createCharacterSheet(){  //wip change from void to File upon development
+        Scanner scanner = new Scanner(System.in); //user input
+        String inputName, inputRace, inputClas, inputBg, response;
+        CharSheet newCharacter = null;
+        while(true){    
+            System.out.println("Welcome to the character sheet creator. We will construct the fundamentals here. Let's start with your character's name.");
+            try{
+                //user input
+                inputName = scanner.nextLine();
+                System.out.println("Next, your character's race");
+                inputRace = scanner.nextLine();
+                System.out.println("Now, your character's starting class");
+                inputClas = scanner.nextLine();
+                System.out.println("Finally, your character's background");
+                inputBg = scanner.nextLine();
+                //CharSheet getMatchingRaceDetails();
+                //CharSheet getMatchingClassDetails();
+                //CharSheet getMatchingBGDetails();
+                newCharacter = new CharSheet(/*plug in here later */);
+                System.out.println("Is this correct? (Y/y to confirm)\n " + newCharacter);
+                response = scanner.nextLine();
+                if(response.equals("Y") || response.equals("y")){
+                    break;
+                }
+                //end user input: replace with all at once editable submission (if no, it cancels and allows edits)
+            }catch(Exception e){
+                System.out.println("Invalid input processing, please try again." + e.getMessage());
+                scanner.nextLine(); //flush
+            }finally{
+                scanner.close();
+            }
+        }
+        if(newCharacter == null){
+            return null;
+        }else{
+            return newCharacter;
+        }
     }
 }
